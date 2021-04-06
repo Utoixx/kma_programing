9540fde043e26d5d50bceadb180ef4f932d40d2e
OEM Configuration (temporary user) <quoctuan2000bg@gmail.com>
2021-04-06 15:53:41 +0700

    none

--- java/final_exam/src/main/java/de1cau1/Client.java
+++ java/final_exam/src/main/java/de1cau1/Client.java
@@ -5,10 +5,52 @@
  */
 package de1cau1;
 
-/**
- *
- * @author quoct
- */
-public class Client {
-    
+import java.io.*;
+import java.util.logging.Logger;
+import java.net.*;
+import java.util.logging.Level;
+
+public class Client{
+    DatagramSocket socket;
+    DatagramPacket packet;
+
+    //Establis connection
+    public void connect(){
+        try{
+            socket= new DatagramSocket();
+        }catch(SocketException e){
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    //Send a message to server
+    public void sendMessage(String message){
+        try{
+            byte[] data= new byte[1024];
+            data=message.getBytes();
+            InetAddress ip= InetAddress.getByName("localhost");
+            packet= new DatagramPacket(data, data.length, ip, 6066);
+            socket.send(packet);
+        }catch(IOException e){
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    //Receive a message from server
+    public String receiveMessage(){
+        try{
+            byte[] data= new byte[1024];
+            packet= new DatagramPacket(data, data.length);
+            socket.receive(packet);
+            String message= new String(packet.getData()).trim();
+            return message;
+        }catch(IOException e){
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, e);
+            return "";
+        }
+    }
+    public static void main(String[] args){
+        Client client= new Client();
+        client.connect();
+        client.sendMessage(Character.toString('a'));
+        System.out.println(client.receiveMessage());
+    }
 }
--- java/final_exam/src/main/java/de1cau1/Server.java
+++ java/final_exam/src/main/java/de1cau1/Server.java
@@ -9,6 +9,50 @@ package de1cau1;
  *
  * @author quoct
  */
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Server {
-    
+    private DatagramSocket socket;
+    private DatagramPacket packet;
+
+    public void connect(){
+        try{
+            socket= new DatagramSocket(6066);
+        }catch(SocketException e){
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    public void sendMessage(String message){
+        try{
+            byte[] data= new byte[1024];
+            data=message.getBytes();
+            InetAddress ip= packet.getAddress();
+            int port= packet.getPort();
+            packet= new DatagramPacket(data, data.length, ip, port);
+            socket.send(packet);
+        }catch(IOException e){
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    public String receiveMessage(){
+        try{
+            byte[] data= new byte[1024];
+            packet= new DatagramPacket(data, data.length);
+            socket.receive(packet);
+            String message= new String(packet.getData()).trim();
+            return message;
+        }catch(IOException e){
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, e);
+            return "";
+        }
+    }
+    public static void main(String[] args){
+        Server sv= new Server();
+        sv.connect();
+        char data=Character.toUpperCase(sv.receiveMessage().charAt(0));
+        sv.sendMessage(Character.toString(data));
+    }
 }
